76aed8ad2d14b41efa01a81c4bb62d5dc34e1ec1
ejasmith <96847300+ejasmith@users.noreply.github.com>
2023-02-25 16:48:05 -0500

    Added basic highlighting and CSS retrieval

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 // Get button
-let btn = document.getElementById("btn")
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
 
 // Run on click
-btn.addEventListener("click", async () => {
+btn1.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }) // Find current tab
 
     chrome.scripting.executeScript({ // Run the following script on our tab
@@ -18,3 +19,53 @@ btn.addEventListener("click", async () => {
         }
     })
 })
+
+// Button to allow user to highlight an element, and when they click it will open window with CSS
+btn2.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            // Highlighting referenced from https://hospodarets.com/highlight_element_with_page_fading
+            // Referenced from this stack overflow: https://stackoverflow.com/questions/4445102/google-chrome-extension-highlight-the-div-that-the-mouse-is-hovering-over
+            
+            var prevHighlight = null;
+
+            document.addEventListener('mousemove', function (e) { // On mouse over
+                let targetHighlight = e.target;
+
+                if (prevHighlight != targetHighlight) {
+                    if (prevHighlight != null) {
+                        prevHighlight.classList.remove('highlight'); // Remove highlight from element user's mouse "left"
+                    }
+
+                    targetHighlight.classList.add('highlight'); // Add highlighting to current element
+
+                    prevHighlight = targetHighlight;
+                }
+            }, false);
+
+            // TODO: Make it so clicking wont activate buttons or hyperlinks on the website
+            // TODO: List of CSS styling is too long, also it uses computed styling which might not be ideal
+            document.addEventListener('click', function (e) {
+                let element = e.target;
+
+                var myWindow = window.open("", "", "width=400,height=400"); // Open new window
+                myWindow.document.body.innerHTML += "<div style='font-family: sans-serif;'></div>"
+
+                if (element != null) {
+                    const styling = window.getComputedStyle(element, null); // Extract styling
+
+                    var cssOutput = "";
+
+                    for (var j = 0; j < styling.length; j++) {
+                        cssOutput += styling[j] + ": " + styling.getPropertyValue(styling[j]) + "<br>"; // Put styling in readable format
+                    }
+
+                    myWindow.document.querySelector('div').innerHTML = "<p> STYLING: <br>" + cssOutput + "</p>"; // Print styling to window
+                }
+            }, false);
+        }
+    });
+});
