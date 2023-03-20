94b566e7935822020c498977a1b6bee9d7678915
ejasmith <96847300+ejasmith@users.noreply.github.com>
2023-03-20 15:11:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -239,7 +239,7 @@ captureButton.addEventListener("click", async () => {
 //     });
 // });
 
-// Button to veiw saved styles
+// Button to view saved styles
 btn4.addEventListener("click", async() => {
     if (display.hidden) {
         display.hidden = false;
@@ -262,6 +262,7 @@ chrome.storage.local.get(null, function(items) { // Start by getting all the key
             const styleDiv = document.createElement("div");
             styleDiv.style = "display: flex;";
             
+            // Add style name
             const text = document.createElement("p");
             text.innerHTML = resultParsed.name
             text.style = "text-align: left; display: inline-block; width: 50%;";
@@ -289,9 +290,12 @@ chrome.storage.local.get(null, function(items) { // Start by getting all the key
                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                 </svg>
             `;
+
             deleteButton.classList.add("button-simple");
             deleteButton.addEventListener("click", async() => {
                 chrome.storage.local.remove([keyCopy]);
+                // TODO: Hide deleted style in list
+                styleDiv.hidden = true;
             });
 
             styleDiv.appendChild(text);
