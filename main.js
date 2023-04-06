1947f367439fbd24e9dc6e780faefb6c277d7b4d
ejasmith <96847300+ejasmith@users.noreply.github.com>
2023-04-06 14:59:43 -0400

    Added renaming feature

--- main.js
+++ main.js
@@ -11,11 +11,15 @@ let dateSortDescButton = document.getElementById("datesortdescbutton");
 let confirmDeleteButton = document.getElementById("confirmdeletebutton");
 let cancelDeleteButton = document.getElementById("canceldeletebutton");
 
+let saveNameButton = document.getElementById("savenamebutton");
+let cancelNameButton = document.getElementById("cancelnamebutton");
+
 // Various display windows
 const styleDisplay = document.getElementById("saved-styles");
 const searchBar = document.getElementById("searchbar");
 const deletePopup = document.getElementById("deletepopup");
 const copyPopup = document.getElementById("copypopup");
+const renamePopup = document.getElementById("renamepopup");
 const disclaimer = document.getElementById("disclaimer");
 
 // Allow access to session storage
@@ -646,10 +650,41 @@ function displayStylesSorted(target, direction) {
                 deletePopup.classList.add("show");
             });
 
+            // Add a rename button to the div
+            const renameButton = document.createElement("button");
+            renameButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
+            `;
+            renameButton.classList.add("button-simple");
+            renameButton.addEventListener("click", async() => {
+                let nameInput = document.getElementById("nameinput");
+                nameInput.value = resultParsed.name;
+                
+                saveNameButton.addEventListener("click", async() => {
+                    let styleNew = { name: nameInput.value, dateSaved: resultParsed.dateSaved, fontCss: resultParsed.fontCss, coloringCss: resultParsed.coloringCss, borderCss: resultParsed.borderCss, positioningCss: resultParsed.positioningCss };
+                    var obj= {};
+                    obj[key] = JSON.stringify(styleNew);
+                    chrome.storage.local.set(obj);
+
+                    text.innerText = nameInput.value;
+                    renamePopup.classList.remove("show");
+                });
+
+                cancelNameButton.addEventListener("click", async() => {
+                    renamePopup.classList.remove("show");
+                });
+
+                renamePopup.classList.add("show");
+            });
+
             styleDiv.appendChild(text);
             buttonContainer.appendChild(copyButton);
             buttonContainer.appendChild(deleteButton);
             buttonContainer.appendChild(previewButton);
+            buttonContainer.appendChild(renameButton);
             styleDiv.appendChild(buttonContainer);
             styleDisplay.appendChild(styleDiv);
         }
