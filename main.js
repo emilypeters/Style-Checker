0213c18c816382c7c4037bd1a9ece00fbc040273
ejasmith <96847300+ejasmith@users.noreply.github.com>
2023-04-08 17:01:58 -0400

    Reverted to old button design

--- main.js
+++ main.js
@@ -352,18 +352,10 @@ function displayStylesSorted(target, direction) {
             const resultParsed = JSON.parse(result.result);
             const key = result.key;
 
-            // Wrapper for buttons and dropdown of buttons
-            const wrapper = document.createElement("div");
-            wrapper.style = "text-align: right;"
-
             // Create div with the style's name
             const styleDiv = document.createElement("div");
             styleDiv.classList.add("style");
             styleDiv.style = "display: flex; align-items: center;";
-
-            // Dropdown for additional buttons
-            const expansion = document.createElement("div");
-            expansion.hidden = true;
             
             // Add style name
             const text = document.createElement("p");
@@ -689,45 +681,15 @@ function displayStylesSorted(target, direction) {
                 renamePopup.classList.add("show");
             });
 
-            const expandButton = document.createElement("button");
-            expandButton.style = "font-size: x-small;"
-            let caretLeft = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
-                    <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-                </svg>
-            `;
-            let caretDown = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                </svg>
-            `;
-            expandButton.innerHTML = caretLeft;
-            expandButton.classList.add("button-simple");
-            expandButton.addEventListener("click", async() => {
-                if (expansion.hidden) {
-                    expansion.hidden = false;
-                    expandButton.innerHTML = caretDown;
-                } else {
-                    expansion.hidden = true;
-                    expandButton.innerHTML = caretLeft;
-                }
-            });
-
-            expansion.appendChild(renameButton);
-            expansion.appendChild(deleteButton);
-
             buttonContainer.appendChild(copyButton);
             buttonContainer.appendChild(previewButton);
-            buttonContainer.appendChild(expandButton);
+            buttonContainer.appendChild(renameButton);
+            buttonContainer.appendChild(deleteButton);
 
             styleDiv.appendChild(text);
             styleDiv.appendChild(buttonContainer);
 
-            wrapper.appendChild(styleDiv);
-            wrapper.appendChild(expansion);
-
-            styleDisplay.appendChild(wrapper);
-            //styleDisplay.appendChild(styleDiv);
+            styleDisplay.appendChild(styleDiv);
         }
     });
 }
