095e246b5a73ad476e06246f296848438077897b
Emily Peters <epete14@u.rochester.edu>
2023-04-10 20:39:12 -0400

    Added edit icon and limitations disclaimer

--- main.js
+++ main.js
@@ -368,7 +368,11 @@ function displayStylesSorted(target, direction) {
 
             // Add edit button
             const editButton = document.createElement("button");
-            editButton.innerHTML = `Edit`;
+            editButton.innerHTML = `
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+               </svg>
+            `;
             editButton.classList.add("button-simple");
             editButton.addEventListener("click", async () => {
                 var editPopup = window.open("", "", "width=400,height=400,toolbar=no,menubar=no");
@@ -411,13 +415,17 @@ function displayStylesSorted(target, direction) {
                           #remove-btn {
                              font-size: 15px;
                           }
+                          #notice {
+                             font-weight: bold;
+                          }
                        </style>
                     </head>
                     <body class="styling">
                          <div id="content"></div>
+                         <p id="notice">Note: entering multiple lines of code is not supported</p>
                          <div class="buttoncontainer">
                             <form id="delete_form">
-                                <label id="remove-btn">Remove : <input id="delete_txt" type="text" placeholder="Ex. font-size: 13px;"/></label>
+                                <label id="remove-btn">Remove: <input id="delete_txt" type="text" placeholder="Ex. font-size: 13px;"/></label>
                                 <button type="submit" id="submit_btn" class="button-simple">Submit</button>
                             </form>
                          </div>
